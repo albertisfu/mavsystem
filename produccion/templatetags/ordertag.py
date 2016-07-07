c869e85f386d24c7795f4025b740788c20f5675d
Alberto <albertisfu@gmail.com>
2016-07-07 03:54:05 -0500

    template tag checkinsumo

---
+++ produccion/templatetags/ordertag.py
@@ -0,0 +1,29 @@
+from django import template
+from produccion.models import *
+#from almacen.models import Insumo
+from django.shortcuts import get_object_or_404
+register = template.Library()
+
+
+@register.simple_tag
+def get_obj(insumo, orden):
+	producto_orden = get_object_or_404(ProductoOrden, pk = orden)
+	insumo_producto =  get_object_or_404(InsumoProducto, pk = insumo)
+	print producto_orden
+	print insumo_producto
+	obj = CheckInsumoProducto.objects.filter(insumo=insumo_producto, productorden=producto_orden).latest('fecha')
+	estatus = obj.estatus
+	print estatus
+	return estatus
+
+
+@register.simple_tag
+def get_fecha(insumo, orden):
+	producto_orden = get_object_or_404(ProductoOrden, pk = orden)
+	insumo_producto =  get_object_or_404(InsumoProducto, pk = insumo)
+	print producto_orden
+	print insumo_producto
+	obj = CheckInsumoProducto.objects.filter(insumo=insumo_producto, productorden=producto_orden).latest('fecha')
+	fecha = obj.fecha
+	print fecha
+	return fecha
